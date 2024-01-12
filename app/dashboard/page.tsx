e258c7be841dd81b2542df359b2c330ad6df6a4d
supakrit.c <supakrit.c@arise.tech>
2024-01-12 15:22:57 +0700

    feat: add loading skeleton

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,11 @@
+import { Suspense } from "react";
 import { fetchCardData, fetchRevenue, fetchLatestInvoices } from "../lib/data";
 import { Card } from "../ui/dashboard/cards";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
+import { CardSkeleton } from "../ui/skeletons";
+import { RevenueChartSkeleton } from "../ui/skeletons";
+import { LatestInvoicesSkeleton } from "../ui/skeletons";
 
 export default async function DashboardPage() {
   const {
@@ -17,18 +21,32 @@ export default async function DashboardPage() {
     <div className="w-full">
       Dashboard
       <div className="gap-4 grid grid-cols-2 md:grid-cols-4 mt-6">
-        <Card title="Collected" value={totalPaidInvoices} type="invoices" />
-        <Card title="Pending" value={totalPendingInvoices} type="invoices" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customer"
-          value={numberOfCustomers}
-          type="invoices"
-        />
+        <Suspense fallback={<CardSkeleton />}>
+          <Card title="Collected" value={totalPaidInvoices} type="invoices" />
+          <Card title="Pending" value={totalPendingInvoices} type="invoices" />
+          <Card
+            title="Total Invoices"
+            value={numberOfInvoices}
+            type="invoices"
+          />
+          <Card
+            title="Total Customer"
+            value={numberOfCustomers}
+            type="invoices"
+          />
+        </Suspense>
       </div>
       <div className="gap-4 grid grid-cols-1 md:grid-cols-2 mt-6">
-        <div><RevenueChart revenue={revenue} /></div>
-        <div><LatestInvoices latestInvoices={latestInvoices} /></div>
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <div>
+            <RevenueChart revenue={revenue} />
+          </div>
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <div>
+            <LatestInvoices latestInvoices={latestInvoices} />
+          </div>
+        </Suspense>
       </div>
     </div>
   );
