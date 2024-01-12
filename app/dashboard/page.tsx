a1818903f62e31b62283467ae51f282c9b4f9c7f
supakrit.c <supakrit.c@arise.tech>
2024-01-12 14:58:21 +0700

    feat: fetch card data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,30 @@
-import { fetchLatestInvoices } from "../lib/data";
+import { fetchCardData, fetchRevenue ,fetchLatestInvoices } from "../lib/data";
 import { Card } from "../ui/dashboard/cards";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 
 export default async function DashboardPage() {
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
 
   return (
     <div className="w-full">
       Dashboard
       <div className="gap-4 grid grid-cols-4 mt-6">
-        <Card title="Collected" value="4000" type="invoices" />
-        <Card title="Pending" value="4000" type="invoices" />
-        <Card title="Total Invoices" value="4000" type="invoices" />
-        <Card title="Total Customer" value="4000" type="invoices" />
+        <Card title="Collected" value={totalPaidInvoices} type="invoices" />
+        <Card title="Pending" value={totalPendingInvoices} type="invoices" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card title="Total Customer" value={numberOfCustomers} type="invoices" />
       </div>
       <div className="gap-4 grid grid-cols-2 mt-6">
         <div>
-          <RevenueChart />
+          <RevenueChart revenue={revenue} />
         </div>
         <div>
           <LatestInvoices latestInvoices={latestInvoices} />
